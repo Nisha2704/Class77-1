bdd2ba89505990e3ec6ca6168019405957dacc2f
Nisha2704 <nisha.sricauvery@gmail.com>
2021-06-12 00:49:27 +0530

    commit class77

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import 'react-native-gesture-handler';
+import HomeScreen from './Screens/HomeScreen';
+import ISSLocationScreen from './Screens/ISSLocationScreen';
+import MeteorScreen from './Screens/MeteorsScreen';
+import UpdateScreen from './Screens/UpdateScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from  '@react-navigation/stack';
+
+const Stack = createStackNavigator()
+
+function App(){
+  return(
+    <NavigationContainer>
+  <Stack.Navigator initialRouteName = "Home" screenOptions={{
+    headerShown:false
+  }}>
+    <Stack.Screen name = "Home" component = {HomeScreen}/>
+    <Stack.Screen name = "ISSLocation" component = {ISSLocationScreen}/>
+    <Stack.Screen name = "Meteors" component = {MeteorScreen}/>
+    <Stack.Screen name = "Updates" component = {UpdateScreen}/>
+  </Stack.Navigator>
+
+    </NavigationContainer>
+  )
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
